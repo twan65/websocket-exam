7ea8099158574171d7678da337265d68adf3f7e6
twan65 <cx.twan65@gmail.com>
2020-09-01 20:28:45 +0900

    Stomp設定

--- src/main/java/com/websocket/config/WebSocketConfig.java
+++ src/main/java/com/websocket/config/WebSocketConfig.java
@@ -2,26 +2,29 @@ package com.websocket.config;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.simp.config.MessageBrokerRegistry;
 import org.springframework.web.socket.WebSocketHandler;
-import org.springframework.web.socket.config.annotation.EnableWebSocket;
-import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
-import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry;
+import org.springframework.web.socket.config.annotation.*;
 
 @RequiredArgsConstructor
 @Configuration
 @EnableWebSocket // WebSocket活性化
-public class WebSocketConfig implements WebSocketConfigurer {
+public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
     private final WebSocketHandler webSocketHandler;
 
     /**
-     * WebSocketに接続するためのendpointは「ws/chat」に設定
-     *
-     * @param registry
+     * pub/subとはメッセージを供給する主体と消費する主体を分離して提供する。
+     * @param config
      */
     @Override
-    public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
-        // ドメインが他のサーバーからも接続できるようにCORS：setAllowedOrigins("*")を追加
-        registry.addHandler(webSocketHandler, "ws/chat").setAllowedOrigins("*");
+    public void configureMessageBroker(MessageBrokerRegistry config) {
+        config.enableSimpleBroker("/sub");
+        config.setApplicationDestinationPrefixes("/pub");
+    }
+
+    @Override
+    public void registerStompEndpoints(StompEndpointRegistry registry) {
+        registry.addEndpoint("ws/chat").setAllowedOrigins("*").withSockJS();
     }
 }
